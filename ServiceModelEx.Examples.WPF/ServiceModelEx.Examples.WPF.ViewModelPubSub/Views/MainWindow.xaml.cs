c9a3c6bdb55cf2cc1e9f31367e94d448bd37ca52
Andrew Hoefling <andrew@hoeflingsoftware.com>
2016-12-20 22:31:46 -0500

    moving MainWindow to Views namespace

---
+++ ServiceModelEx.Examples.WPF/ServiceModelEx.Examples.WPF.ViewModelPubSub/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ServiceModelEx.Examples.WPF.ViewModelPubSub.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
